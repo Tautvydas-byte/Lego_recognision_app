5afaddec04787407cbccbeb279f4e71cd4ed8d42
Tautvydas <tautvydas.kvietkauskas@gmail.com>
2022-03-09 00:11:37 +0200

    Get the webcam

--- main.py
+++ main.py
@@ -25,17 +25,33 @@ import cv2  # helps access the webcam and render feeds
 class LegoApp(App):
 	def build(self):
 		# Main layout components
-		self.img1 = Image(size_hint=(1, .8))  # 80proc height of layer img size
+		self.web_cam = Image(size_hint=(1, .8))  # 80proc height of layer img size
 		self.button = Button(text="Verify", size_hint=(1, .1))  # button 10proc size
 		self.verification = Label(text="Verification Uninitiated", size_hint=(1, .1))  # verified or not 10proc size
 
 		# Add items to layout
 		layout = BoxLayout(orientation='vertical')
-		layout.add_widget(self.img1)
+		layout.add_widget(self.web_cam)
 		layout.add_widget(self.button)
 		layout.add_widget(self.verification)
 
+		# Setup video capture device
+		self.capture = cv2.VideoCapture(0)  # reikia pasizaist kuris skaicius jog pasiektu tinkama webcam
+		Clock.schedule_interval(self.update, 1.0/33.0)#Update function running to interval of time
+
 		return layout
+
+	# Run continuously to get webcam feed
+	def update(self, *args):
+		# Read frame from opencv
+		ret, frame = self.capture.read()  # reading a frame
+		frame = frame[120:120 + 250, 200: 200 + 250,:]  # cutting down to 120 width and 250 height
+
+		# Flip horizontal and convert raw OpenCV2 image array to texture for rendering. Then setting our image equal to that texture
+		buf = cv2.flip(frame, 0).tostring()#flip image to horizontal and convert to string
+		img_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')#allow convert image to texture kivy documentation
+		img_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')#taking image converting to texture and render to app
+		self.web_cam.texture = img_texture#setting web_cam image texture to image texture
 
 
 if __name__ == '__main__':
